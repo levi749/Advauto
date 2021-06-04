e0910396cff3521336c7ff7d07bb1e3bc5c1e342
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-04 09:06:08 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = f"{file_name} /n @CLALL1",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '😜 JOIN CHANNEL 😜', url="https://t.me/CLALL1"
                                 )
                         ]
                     ]
