e57456a644fb64332b77d1ccca80e3d0309f8b0b
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-04 09:08:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/CL_ALL1"
                                 )
                         ]
                     ]
